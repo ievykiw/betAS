dcb53dc5c9bcb598883b5da4da7a0c90d8d9f223
Andre-CSF <andre.valente594@gmail.com>
2025-08-17 19:52:51 -0300

    Estado prévio de coleta dos dados de cadastro e definição de tipos do usuário

--- src/main/java/com/bet/models/Cliente.java
+++ src/main/java/com/bet/models/Cliente.java
@@ -1,5 +1,95 @@
 package com.bet.models;
 
-public class Cliente {
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
+public class Cliente {
+    private BigInteger id;
+    private BigDecimal saldo;
+    private BigInteger token;
+
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String telefone;
+
+
+    public Cliente() {}
+
+    public Cliente(String nome, String sobrenome, String cpf,
+        String email, String senha, String telefone) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public BigInteger getToken() {
+        return token;
+    }
+    public void setToken(BigInteger token) {
+        this.token = token;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
+
+
